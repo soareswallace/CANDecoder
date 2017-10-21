9f7ec8024d4a672d0eedec7f23e219010f00b627
soareswallace <wssj2@cin.ufpe.br>
2017-10-21 13:38:35 -0300

    Bit timing

--- bitTiming.sv
+++ bitTiming.sv
@@ -6,3 +6,11 @@ module bitTiming(
 reg [8:0] cont;
 reg [7:0] estado_atual;
 
+parameter baudrate_prescaler = 9600,
+            oscilator_frequency = 4000; //aleatory values
+
+
+always_ff @(posedge reset or negedge clk) begin
+    
+end
+
