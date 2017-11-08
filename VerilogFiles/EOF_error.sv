3697d9b4e320c3924c672242f5c6ef7ad3a6b795
soareswallace <wssj2@cin.ufpe.br>
2017-11-07 21:49:12 -0300

    Better modularization

--- VerilogFiles/EOF_error.sv
+++ VerilogFiles/EOF_error.sv
@@ -3,7 +3,7 @@ module EOF_error(input reset, SP, RX, EOF_Flag,
                );
 
 reg [7:0]estado_atual;
-parameter sts1 = 0, sts2 = 1;
+parameter sts1 = 0, sts2 = 1, sts3 = 2;
 
 initial cont = 9'd0;
 initial EOF_Error = 1'b1;
@@ -23,22 +23,22 @@ always_ff @ (posedge SP or posedge reset)begin
                 cont <= 9'd0;
             end
         end
-
         sts2: begin  //EOF Area
-            if(EOF_Flag == 1'b1)begin
-                estado_atual <= sts1;
+            if(cont == 5)begin
+                estado_atual <= sts3;
                 cont <= 9'd0;
             end
-            else begin
-                if(cont<=5 && RX==0)begin //EOF Error found
-                    EOF_Error <= 1'b0;
-                    cont <= 9'd0;
-                    estado_atual <= sts1;
-                end
-                else begin
-                    cont <= cont + 9'd1;
-                end
+            if (RX ==0) begin
+                EOF_Error <= 1'b0;
+                cont <= 9'd0;
+                estado_atual <= sts1;
             end
+            else begin
+                cont <= cont + 9'd1;
+            end
+        end
+        sts3: begin
+            estado_atual <= sts1;
         end
     endcase
 end
